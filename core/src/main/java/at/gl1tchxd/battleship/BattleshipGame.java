a287e1e6ba012fb06f5bec2838b324c64838b5b9
gl1tchxd <contact@gl1tchxd.at>
2025-09-18 22:57:54 +0200

    gradle build system working now

--- core/src/main/java/at/gl1tchxd/battleship/BattleshipGame.java
+++ core/src/main/java/at/gl1tchxd/battleship/BattleshipGame.java
@@ -1,4 +1,36 @@
 package at.gl1tchxd.battleship;
 
-public class BattleshipGame {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class BattleshipGame extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // Uses libGDX's default Arial font
+
+        // Set the initial screen (main menu, game screen, etc.)
+        // this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        // Clear the screen with dark blue color
+        Gdx.gl.glClearColor(0.1f, 0.2f, 0.4f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        super.render(); // Important: this calls the active screen's render method
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 }
